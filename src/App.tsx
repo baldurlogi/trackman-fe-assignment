0d9f87eb82e027aaf780a1b3a41545134d01db44
BaldurLogi <baldurlogi16@icloud.com>
2025-09-18 22:23:57 +0200

    chore(theme): add colors/typography/spacing and tailwind extentions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-300">Hello Tailwind v4.1</h1>
+      <h1 className="font-bold text-error bg-error-light rounded-2xl">Hello Tailwind v4.1</h1>
     </>
   );
 }
